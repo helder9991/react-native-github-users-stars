2e91084d8674b045966f9fccb7d2ba3a0a403c2d
Hélder <helderafonsoss@hotmail.com>
2019-11-18 19:50:14 -0300

    add reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+// Importação do Reactotron
+import './config/ReactotronConfig';
+
 // import { Container } from './styles';
 
 export default function App() {
